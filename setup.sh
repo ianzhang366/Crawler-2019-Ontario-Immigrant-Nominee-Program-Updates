7efc707853de9171282e4e0c692e526a985141ac
Ian Zhang <ian.w.zhang@ibm.com>
2018-11-21 16:37:40 -0500

    adding set up script

---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+install_docker() {
+		echo -e "\n\nInstalling Docker\n";
+		curl -fsSL get.docker.com -o get-docker.sh;
+		sh get-docker.sh;
+		rm get-docker.sh;
+}
+
+install_docker_compose() {
+		echo -e "\n\nInstalling Docker-Compose\n";
+		sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose;
+		sudo chmod +x /usr/local/bin/docker-compose;
+}
+
+
+main() {
+		ENV=$1;
+		VERSION_WITH_PERIODS=$2;
+        echo -e "$1\t$2"
+		SCRIPT_START_TIME=`date`;
+		
+		install_docker
+		install_docker_compose
+
+		
+		SCRIPT_END_TIME=`date`;
+		echo -e "\n\nScript Start Time: ${SCRIPT_START_TIME}";
+		echo -e "Script End Time: ${SCRIPT_END_TIME}";
+		echo -e "\n\n\nProject ENV Set up Completed.\n";
+}
+
+main "$1" "$2"
