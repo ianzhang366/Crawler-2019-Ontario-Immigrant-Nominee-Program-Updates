c5e964ec94b0a58bc9e0760ec15147a522c0f5b0
wzhan366 <wzhan366@fyre.ibm.com>
2018-11-22 09:27:39 -0800

    adding docker logics

--- run_image.sh
+++ run_image.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 #this script is load the image built by build_docker_image.sh and run it
-
+echo -e 'Need 2 parameters to run.\n Para1: indicate the image version. \n Para2: indicate the container name.\n'
 VERSION=$1;
+C_NAME=$2;
+
+echo -e "Version: ${VERSION} \t C_NAME: ${C_NAME}"
 #adding the project to image
-doker kill pnp_crawler 
-docker run -dit -p 8888:8888 -name pnp_crawler pnp_crawler:${VERSION}
+docker ps 
+if docker ps --filter "name=${C_NAME}" | grep -i "${C_NAME}"; then
+	echo -e "Killing the previous running container"
+	docker kill ${C_NAME}
+	echo -e "Removing /${C_NAME}"
+	docker rm "/${C_NAME}"
+fi 
+docker run -dit -p 8888:8888 --name ${C_NAME} pnp_crawler:${VERSION}
+docker ps --filter "name=${C_NAME}"
