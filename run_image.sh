a6e226b7200a091d7f4ea6450451c60b518e9227
Ian Zhang <ian.w.zhang@ibm.com>
2018-11-25 21:00:59 -0500

    modify run script

--- run_image.sh
+++ run_image.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #this script is load the image built by build_docker_image.sh and run it
 echo -e 'Need 2 parameters to run.\n Para1: indicate the image version. \n Para2: indicate the container name.\n'
-VERSION=$1;
-C_NAME=$2;
+
+C_NAME=$1;
+VERSION=$2;
 
 echo -e "Version: ${VERSION} \t C_NAME: ${C_NAME}"
 #adding the project to image
@@ -13,5 +14,5 @@ if docker ps --filter "name=${C_NAME}" | grep -i "${C_NAME}"; then
 	echo -e "Removing /${C_NAME}"
 	docker rm "/${C_NAME}"
 fi 
-docker run -dit -p 8888:8888 --name ${C_NAME} ${C_NAME}:${VERSION}
+docker run -dit -p 8888:8888 --name "${C_NAME}_${VERSION}" ${C_NAME}:${VERSION}
 docker ps --filter "name=${C_NAME}"
