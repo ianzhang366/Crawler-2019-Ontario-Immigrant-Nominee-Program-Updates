9f7229c419bc0309f2bd2d54aaf9f680352c125c
Ian Zhang <ian.w.zhang@ibm.com>
2018-11-25 21:03:56 -0500

    modify run script

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 #this script is used to build the docker image from the Dockerfile 
 
-VERSION=$1;
+C_NAME=$1;
+VERSION=$2;
+
 SCRIPT_START_TIME=`date`;
 echo -e "\n\n Build docker image script start time: ${SCRIPT_START_TIME}";
 
-docker build -t pnp_crawler:${VERSION} -f /home/pnpCrawler/Dockerfile . 
+docker build -t ${C_NAME}:${VERSION} -f /home/pnpCrawler/Dockerfile . 
 
 SCRIPT_COMPLETE_TIME=`date`;
 echo -e "\n\n Build docker image script complete time: ${SCRIPT_COMPLETE_TIME}";
